813bb805a02d9369dc0971ed41a42d707de1505b
Michael Lodder <redmike7@gmail.com>
2020-04-06 18:06:38 -0600

    Update
    Signed-off-by: Michael Lodder <redmike7@gmail.com>

--- __tests__/bls12381.spec.ts
+++ __tests__/bls12381.spec.ts
@@ -1,9 +1,18 @@
+import { randomBytes } from "@stablelib/random";
 import { generateKeyPair } from "../src/bls12381";
 
 describe("bls12381", () => {
   it("should be able to generate a key pair", () => {
     const result = generateKeyPair();
     expect(result).toBeDefined();
+    expect(result.publicKey).toBeDefined();
+    expect(result.secretKey).toBeDefined();
+    expect(result.secretKey.length).toEqual(48);
+    expect(result.publicKey.length).toEqual(192);
+  });
+  it("should be able to generate a key pair with a seed", () => {
+    const seed = randomBytes(50);
+    const result = generateKeyPair(seed);
     expect(result.publicKey).toBeDefined();
     expect(result.secretKey).toBeDefined();
     expect(result.secretKey.length).toEqual(48);
--- src/bls12381.ts
+++ src/bls12381.ts
@@ -1,10 +1,11 @@
 import { BlsKeyPair } from "./types";
 const zmix = require("../native/index.node");
 
-export const generateKeyPair = (): BlsKeyPair => {
-  const result = zmix.bls_generate_key();
+export const generateKeyPair = (seed?: Uint8Array): BlsKeyPair => {
+  const result = seed ? zmix.bls_generate_key(seed?.buffer) : zmix.bls_generate_key();
   return {
     publicKey: new Uint8Array(result.publicKey),
     secretKey: new Uint8Array(result.secretKey)
   };
 };
+
