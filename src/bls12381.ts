34f85c5dedda4bb228e78ee50985a6ab6d19f2d1
Tobias <tplooker@gmail.com>
2020-04-07 20:20:15 +1200

    feat: minor tweaks to package.json and lint issue

--- src/bls12381.ts
+++ src/bls12381.ts
@@ -1,11 +1,13 @@
 import { BlsKeyPair } from "./types";
+// tslint:disable-next-line
 const zmix = require("../native/index.node");
 
 export const generateKeyPair = (seed?: Uint8Array): BlsKeyPair => {
-  const result = seed ? zmix.bls_generate_key(seed?.buffer) : zmix.bls_generate_key();
+  const result = seed
+    ? zmix.bls_generate_key(seed?.buffer)
+    : zmix.bls_generate_key();
   return {
     publicKey: new Uint8Array(result.publicKey),
     secretKey: new Uint8Array(result.secretKey)
   };
 };
-
