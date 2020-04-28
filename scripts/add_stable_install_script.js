a6149d6bfbe2c7aba4ac64bd7484f97dd1a9d0f8
Tobias Looker <tobias.looker@mattr.global>
2020-04-28 16:31:02 +1200

    ci: update publish scripts (#88)

--- scripts/add_stable_install_script.js
+++ scripts/add_stable_install_script.js
@@ -1,15 +1,9 @@
-'use strict';
+"use strict";
 
-const fs = require('fs');
-let packageJson = require('../package.json');
+const fs = require("fs");
+let packageJson = require("../package.json");
 
 // Add the post install script that will fetch the native node module from gh packages
 packageJson.scripts.install = "node-pre-gyp install --fallback-to-build=false";
 
-// Update the package json so that the RUST is not compiled into the released package
-packageJson.files = [
-    "lib",
-    "native/index.node"
-];
-
-fs.writeFileSync('package.json', JSON.stringify(packageJson, null, 2));
+fs.writeFileSync("package.json", JSON.stringify(packageJson, null, 2));
