50c716feef4dacacd568c406e19955a563a85792
Tobias Looker <tobias.looker@mattr.global>
2020-07-13 14:48:03 +1200

    docs: apply suggested changes to api docs
    Co-authored-by: Kyle Den Hartog <kdenhartog@users.noreply.github.com>

--- native/src/lib.rs
+++ native/src/lib.rs
@@ -125,7 +125,7 @@ fn bls_public_key_to_bbs_key(mut cx: FunctionContext) -> JsResult<JsArrayBuffer>
 /// {
 ///     "secretKey": ArrayBuffer                // The private key of signer
 ///     "publicKey": ArrayBuffer                // The public key of signer
-///     "messages": [ArrayBuffer, ArrayBuffer], // The messages to be signed as strings. They will be hashed with Blake2b
+///     "messages": [ArrayBuffer, ArrayBuffer], // The messages to be signed as ArrayBuffers. They will be hashed with Blake2b
 /// }
 ///
 /// `return`: `ArrayBuffer` the signature
@@ -170,7 +170,7 @@ fn bbs_sign(mut cx: FunctionContext) -> JsResult<JsArrayBuffer> {
 /// {
 ///     "publicKey": ArrayBuffer                // The public key
 ///     "signature": ArrayBuffer                // The signature
-///     "messages": [ArrayBuffer, ArrayBuffer], // The messages that were signed as strings. They will be Blake2b hashed
+///     "messages": [ArrayBuffer, ArrayBuffer], // The messages that were signed as ArrayBuffers. They will be Blake2b hashed
 /// }
 ///
 /// `return`: true if valid `signature` on `messages`
@@ -219,7 +219,7 @@ fn bbs_verify(mut cx: FunctionContext) -> JsResult<JsBoolean> {
 /// The context object model is as follows:
 /// {
 ///     "publicKey": ArrayBuffer                // The public key of signer
-///     "messages": [ArrayBuffer, ArrayBuffer], // The messages that will be blinded as strings. They will be Blake2b hashed
+///     "messages": [ArrayBuffer, ArrayBuffer], // The messages that will be blinded as ArrayBuffers. They will be Blake2b hashed
 ///     "blinded": [Number, Number],            // The zero based indices to the generators in the public key for the messages.
 ///     "nonce": ArrayBuffer                    // This is an optional nonce from the signer and will be used in the proof of committed messages if present. It is strongly recommend that this be used.
 /// }
@@ -632,7 +632,7 @@ struct CreateProofContext {
 /// {
 ///     "proof": ArrayBuffer,                   // The proof from `bbs_create_proof`
 ///     "publicKey": ArrayBuffer,               // The public key of the signer in BLS form
-///     "messages": [ArrayBuffer, ArrayBuffer]  // The revealed messages as strings. They will be Blake2b hashed.
+///     "messages": [ArrayBuffer, ArrayBuffer]  // The revealed messages as ArrayBuffers. They will be Blake2b hashed.
 ///     "nonce": ArrayBuffer                    // This is an optional nonce from the verifier and will be used in the proof of committed messages if present. It is strongly recommend that this be used.
 /// }
 ///
@@ -654,7 +654,7 @@ fn bls_verify_proof(mut cx: FunctionContext) -> JsResult<JsBoolean> {
 /// {
 ///     "proof": ArrayBuffer,                   // The proof from `bbs_create_proof`
 ///     "publicKey": ArrayBuffer,               // The public key of the signer
-///     "messages": [ArrayBuffer, ArrayBuffer]  // The revealed messages as strings. They will be Blake2b hashed.
+///     "messages": [ArrayBuffer, ArrayBuffer]  // The revealed messages as ArrayBuffers. They will be Blake2b hashed.
 ///     "nonce": ArrayBuffer                    // This is an optional nonce from the verifier and will be used in the proof of committed messages if present. It is strongly recommend that this be used.
 /// }
 ///
